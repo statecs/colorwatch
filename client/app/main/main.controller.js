4854a2013bed7f585bdeb90fc974f9548a4e96ab
statecs <statec@icloud.com>
2016-04-15 18:02:43 +0200

    [FIX] CSS topbar, settings and dashboard.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('colorwatchApp')
   .controller('MainCtrl', function ($scope, $q, $http, $location, $rootScope) {
-    $scope.descriptionText = 'Här följer tio enkla frågor på hur du uppfattar texter och färgers läsbarhet på webben. Detta för att kunna underlätta för Funka.nu att samla in relevant data kring färgkontraster. Du väljer den du tycker är bäst genom att klicka på respektive bild i formuläret. Uppskattad tid ca 10 min.';
+    $scope.descriptionText = 'Här följer tio enkla frågor på hur du uppfattar texter och färgers läsbarhet på webben. Detta för att kunna underlätta för Funka att samla in relevant data kring färgkontraster. Du väljer den du tycker är bäst genom att klicka på respektive bild i formuläret. Uppskattad tid ca 10 min.';
 
    $rootScope.amt = 0;
 
