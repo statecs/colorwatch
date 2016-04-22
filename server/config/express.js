f2024c3ba404cd9d9363b63e85314d4e055ea5d8
statecs <statec@icloud.com>
2016-04-22 14:15:25 +0200

    [Update] Swagger API-docs
    Much better interface for the api-documentation :)

--- server/config/express.js
+++ server/config/express.js
@@ -18,6 +18,9 @@ var passport = require('passport');
 var session = require('express-session');
 var mongoStore = require('connect-mongo/es5')(session);
 var mongoose = require('mongoose');
+var argv = require('minimist')(process.argv.slice(2));
+var swagger = require("swagger-node-express");
+var bodyParser = require( 'body-parser' );
 
 module.exports = function(app) {
   var env = app.get('env');
@@ -31,6 +34,32 @@ module.exports = function(app) {
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
+ 
+  var subpath = express();
+  app.use(bodyParser());
+  app.use("/v1", subpath);
+
+  swagger.setAppHandler(subpath);
+
+  swagger.setApiInfo({
+        title: "example API",
+        description: "API to do something, manage something...",
+        termsOfServiceUrl: "",
+        contact: "yourname@something.com",
+        license: "",
+        licenseUrl: ""
+    });
+
+       // Set api-doc path
+    swagger.configureSwaggerPaths('', 'api-docs', '');
+
+    // Configure the API domain
+    var domain = 'localhost';
+    if(argv.domain !== undefined)
+        domain = argv.domain;
+    else
+        console.log('No --domain=xxx specified, taking default hostname "localhost".')
+
 
   // Persist sessions with mongoStore
   // We need to enable sessions for passport twitter because its an oauth 1.0 strategy
@@ -45,6 +74,9 @@ module.exports = function(app) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
+    app.get('/api-docs', function (req, res) {
+         res.sendFile(config.root + '/client/api-docs/index.html');
+      });
     app.use(morgan('dev'));
   }
 
@@ -54,6 +86,9 @@ module.exports = function(app) {
     app.use(express.static(path.join(config.root, 'client')));
     app.set('appPath', 'client');
     app.use(morgan('dev'));
+    app.get('/api-docs', function (req, res) {
+         res.sendFile(config.root + '/public/api-docs/index.html');
+      });
     app.use(errorHandler()); // Error handler - has to be last
   }
 };
