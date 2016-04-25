50d662867bf642459dc20d6411d516f21062e341
Joakim Larsson <joakimlarsson92@gmail.com>
2016-04-25 16:14:50 +0200

    New version

--- public/bower_components/angular-spectrum-colorpicker/dist/angular-spectrum-colorpicker.js
+++ public/bower_components/angular-spectrum-colorpicker/dist/angular-spectrum-colorpicker.js
@@ -1,5 +1,5 @@
 /*!
- * angular-spectrum-colorpicker v1.4.3
+ * angular-spectrum-colorpicker v1.4.4
  * https://github.com/Jimdo/angular-spectrum-colorpicker
  *
  * Angular directive for a colorpicker, that bases on http://bgrins.github.io/spectrum/
