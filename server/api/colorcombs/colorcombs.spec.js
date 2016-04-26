c9adacbac09df85c6615dadd82d1d803582011c5
Joakim Larsson <joakimlarsson92@gmail.com>
2016-04-26 11:46:40 +0200

    Removed tests for api - again

--- server/api/colorcombs/colorcombs.spec.js
+++ server/api/colorcombs/colorcombs.spec.js
@@ -4,17 +4,3 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/colorcombs', function() {
-
-  it('NO TESTING', function(done) {
-    /*request(app)
-      .get('/api/colorcombs')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
-      });*/
-  });
-});
--- server/api/result/result.spec.js
+++ server/api/result/result.spec.js
@@ -4,17 +4,3 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/results', function() {
-
-  it('`NO TESTING', function(done) {
-    /*request(app)
-      .get('/api/results')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
-        done();
-      });*/
-  });
-});
